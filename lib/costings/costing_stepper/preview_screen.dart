929fcc26bf470b41c905a13c104e50c032e7b73d
tulsi <tulsi.code@gmail.com>
2025-12-07 14:03:41 +0530

    Preview screen modified

--- lib/costings/costing_stepper/preview_screen.dart
+++ lib/costings/costing_stepper/preview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:costing_master/costings/notifier/selected_client_notifier.dart';
 import 'package:costing_master/model/costing.dart';
 import 'package:costing_master/model/preview_model.dart';
+import 'package:costing_master/domain/enums.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -28,8 +29,6 @@ class Preview extends ConsumerStatefulWidget {
 }
 
 class _PreviewState extends ConsumerState<Preview> {
-  late final TextEditingController shadowDiamondCosting;
-
   @override
   void initState() {
     super.initState();
@@ -46,7 +45,7 @@ class _PreviewState extends ConsumerState<Preview> {
       ByteData? byteData =
           await (image.toByteData(format: ui.ImageByteFormat.png));
       if (byteData != null) {
-        final result = await ImageGallerySaverPlus.saveImage(
+        await ImageGallerySaverPlus.saveImage(
             quality: 100, byteData.buffer.asUint8List());
       }
     }
@@ -55,122 +54,340 @@ class _PreviewState extends ConsumerState<Preview> {
 
     widget.previewModel.previewMap.forEach(
       (chargeType, previewRowModel) {
+        // Determine background color based on charge type
+        Color? backgroundColor;
+        Color? textColor;
+        FontWeight fontWeight = FontWeight.normal;
+        
+        if (chargeType == ChargeType.totalCharges || 
+            chargeType == ChargeType.totalChargesPlusVatavAmount) {
+          backgroundColor = Colors.yellow.shade100;
+          fontWeight = FontWeight.bold;
+        } else if (chargeType == ChargeType.totalChargesPlusVatavPlusProfit) {
+          backgroundColor = Colors.green.shade100;
+          fontWeight = FontWeight.bold;
+          textColor = Colors.green.shade900;
+        } else if (chargeType == ChargeType.vatavAmount || 
+                   chargeType == ChargeType.profitAmount) {
+          fontWeight = FontWeight.w500;
+        }
+
         tableRowWidgets.add(
-          Table(
-              border: TableBorder
-                  .all(),
+          Container(
+            color: backgroundColor,
+            child: Table(
+              border: TableBorder(
+                horizontalInside: BorderSide(
+                  color: Colors.grey.shade300,
+                  width: 0.5,
+                ),
+                bottom: chargeType == ChargeType.totalChargesPlusVatavPlusProfit
+                    ? BorderSide(color: Colors.grey.shade400, width: 2)
+                    : BorderSide.none,
+              ),
+              columnWidths: const {
+                0: FlexColumnWidth(2.5),
+                1: FlexColumnWidth(1.5),
+                2: FlexColumnWidth(1.5),
+              },
               children: [
-                TableRow(children: [
-                  Text(
-                    previewRowModel.title,
-                  ),
-                  Text(
-                    previewRowModel.charge,
-                  ),
-                  Text(
-                    previewRowModel.totalCharge,
-                  ),
-                ]),
-              ]),
+                TableRow(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 10.0,
+                      ),
+                      child: Text(
+                        previewRowModel.title,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: fontWeight,
+                          color: textColor,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 10.0,
+                      ),
+                      child: Text(
+                        previewRowModel.charge,
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: fontWeight,
+                          color: textColor,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 10.0,
+                      ),
+                      child: Text(
+                        previewRowModel.totalCharge,
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: fontWeight,
+                          color: textColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         );
       },
     );
     return SafeArea(
       child: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
               RepaintBoundary(
                 key: globalKey,
                 child: Container(
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
+                    border: Border.all(
+                      color: Colors.grey.shade400,
+                      width: 1.5,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
                     color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade300,
+                        blurRadius: 4,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
                   ),
                   child: Column(
                     children: [
-                      ListTile(
-                        title: Row(
-  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  children: widget.costing != null
-      ? [
-          Column(
-            crossAxisAlignment:
-                CrossAxisAlignment.start,
-            children: [
-              const Text("Sari Name",
-                  style: TextStyle(fontSize: 15)),
-              Text(
-                widget.costing!.sariName.toString(),
-                style: const TextStyle(fontSize: 25),
-              ),
-            ],
-          ),
-          const SizedBox(width: 10),
-          if (widget.costing!.designNo != null)
-            Column(
-              children: [
-                const Text("Design No.",
-                    style: TextStyle(fontSize: 15)),
-                Text(widget.costing!.designNo.toString(),
-                    style: const TextStyle(fontSize: 25)),
-              ],
-            ),
-        ]
-      : [],
-),
-                        subtitle: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: widget.costing != null
-                              ? [
-                                  Text(ref.read(selectedClientProvider)!.name,
-                                      style: const TextStyle(fontSize: 23)),
-                                  Center(
-                                    child: Image.network(
-                                      widget.costing!.imageUrl,
-                                      height: 200,
+                      // Header Section
+                      Container(
+                        padding: const EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: Colors.blue.shade50,
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Colors.grey.shade300,
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                        child: widget.costing != null
+                            ? Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              "Sari Name",
+                                              style: TextStyle(
+                                                fontSize: 12,
+                                                color: Colors.grey.shade700,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                            const SizedBox(height: 4),
+                                            Text(
+                                              widget.costing!.sariName.toString(),
+                                              style: const TextStyle(
+                                                fontSize: 22,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      if (widget.costing!.designNo != null) ...[
+                                        const SizedBox(width: 16),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.end,
+                                          children: [
+                                            Text(
+                                              "Design No.",
+                                              style: TextStyle(
+                                                fontSize: 12,
+                                                color: Colors.grey.shade700,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                            const SizedBox(height: 4),
+                                            Text(
+                                              widget.costing!.designNo.toString(),
+                                              style: const TextStyle(
+                                                fontSize: 22,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ],
+                                  ),
+                                  const SizedBox(height: 12),
+                                  Text(
+                                    ref.read(selectedClientProvider)!.name,
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.grey.shade800,
                                     ),
                                   ),
-                                ]
-                              : [],
+                                ],
+                              )
+                            : const SizedBox.shrink(),
+                      ),
+                      // Image Section
+                      if (widget.costing != null)
+                        Container(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Center(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.network(
+                                widget.costing!.imageUrl,
+                                height: 200,
+                                fit: BoxFit.contain,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Container(
+                                    height: 200,
+                                    color: Colors.grey.shade200,
+                                    child: const Icon(
+                                      Icons.image_not_supported,
+                                      size: 48,
+                                      color: Colors.grey,
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      // Table Header
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade100,
+                          border: Border(
+                            bottom: BorderSide(
+                              color: Colors.grey.shade300,
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                        child: Table(
+                          columnWidths: const {
+                            0: FlexColumnWidth(2.5),
+                            1: FlexColumnWidth(1.5),
+                            2: FlexColumnWidth(1.5),
+                          },
+                          children: [
+                            TableRow(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 12.0,
+                                    vertical: 12.0,
+                                  ),
+                                  child: Text(
+                                    'Items',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey.shade800,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 12.0,
+                                    vertical: 12.0,
+                                  ),
+                                  child: Text(
+                                    'Rate',
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey.shade800,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 12.0,
+                                    vertical: 12.0,
+                                  ),
+                                  child: Text(
+                                    'Total',
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey.shade800,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
-                      Table(
-                        border: TableBorder.all(),
-                        children: const [
-                          TableRow(children: [
-                            Text('Items'),
-                            Text('Rate'),
-                            Text('Total'),
-                          ]),
-                        ],
-                      ),
+                      // Table Rows
                       ...tableRowWidgets,
                     ],
                   ),
                 ),
               ),
+              const SizedBox(height: 16),
+              // Save Button
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  ElevatedButton(
-                      onPressed: () async {
-                        await showPicker();
+                  ElevatedButton.icon(
+                    onPressed: () async {
+                      await showPicker();
 
-                        if (widget.isFromStepper) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text("Image saved to gallery"),
-                              duration: Duration(seconds: 2),
-                            ),
-                          );
-                        } else {
-                          if (mounted)
-                            Navigator.of(context, rootNavigator: true).pop();
-                        }
-                      },
-                      child: const Text("SAVE IMAGE")),
+                      if (widget.isFromStepper) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Image saved to gallery"),
+                            duration: Duration(seconds: 2),
+                          ),
+                        );
+                      } else {
+                        if (mounted)
+                          Navigator.of(context, rootNavigator: true).pop();
+                      }
+                    },
+                    icon: const Icon(Icons.save),
+                    label: const Text("SAVE IMAGE"),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 12,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ],
